0afd7c0ab9139a88b4ae1d3ba0396c779549bc98
Johnny <evgkisevgkis@gmail.com>
2023-06-19 20:57:50 +0500

    Написание функции вывода подготовленных данных

--- coursework_3/utils.py
+++ coursework_3/utils.py
@@ -18,3 +18,34 @@ def sort_data(data):
     sorted_data.sort(key=operator.itemgetter('date'))
     sorted_data = sorted_data[:5]
     return sorted_data
+
+
+def prepare_data(data):
+    """Подготовка данных для отображения"""
+    output = ''
+    for i in data:
+        output += f"{i['date'][8:10]}.{i['date'][5:7]}.{i['date'][:4]} {i['description']} \n"
+        to = i['to'].split()
+        try:
+            from_ = i['from'].split()
+            for x in from_:
+                if x.isalpha():
+                    output += f"{x} "
+                else:
+                    if len(x) == 16:
+                        output += f"{x[:4]} {x[4:6]}** **** {x[:4]} -> "
+                    else:
+                        output += f"**{x[:4]} -> "
+        except KeyError:
+            pass
+        for y in to:
+            if y.isalpha():
+                output += f"{y} "
+            else:
+                if len(y) == 16:
+                    output += f"{y[:4]} {y[4:6]}** **** {y[:4]} \n"
+                else:
+                    output += f"**{y[:4]} \n"
+        output += f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']} \n"
+        output += '\n'
+    return output
